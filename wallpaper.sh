5ed35f156a58764fdd5b74a3d0e4b0f11619a0b9
Gavin Turner <service.haiku882@passinbox.com>
2025-12-09 22:54:04 -0800

    Updated hyprland config: Nvidia env vars, swww wallpapers, screenshots, keybinds

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 WALLPAPER_DIR="$HOME/Pictures/Backgrounds/GruvBox"
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) | shuf -n 1)
+
+# Set wallpaper
 swww img "$WALLPAPER" --transition-type grow --transition-duration 1
+
+# Update hyprlock config
+sed -i "s|path = .*|path = $WALLPAPER|" ~/.config/hypr/hyprlock.conf
