d718004dc2513bd4c845438c9d3263f6ba0038f2
Gavin Turner <service.haiku882@passinbox.com>
2026-05-20 10:48:37 -0700

    Add Fish config with eza, bat, zoxide, fzf

---
+++ fish/conf.d/eza.fish
@@ -0,0 +1,4 @@
+alias ls="eza -F --icons --group-directories-first"
+alias la="eza -AF --icons --group-directories-first"
+alias ll="eza -lAF --icons --group-directories-first"
+alias tree="eza -AF --icons --tree"
---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+starship init fish | source
+zoxide init fish --cmd cd | source
+set -g fish_greeting
+
+# Generated for envman. Do not edit.
+test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
